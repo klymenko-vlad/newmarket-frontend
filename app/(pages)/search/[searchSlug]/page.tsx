bf7caa3bb77c1410edc422e2c14d05a653410ae0
Vlad Klymenko <klymenvlad@gmail.com>
2024-09-06 18:32:26 +0200

    Search page metadata is improved

--- app/(pages)/search/[searchSlug]/page.tsx
+++ app/(pages)/search/[searchSlug]/page.tsx
@@ -1,10 +1,17 @@
-import type { Metadata } from "next";
 import InfinitiveScroll from "@/components/Common/InfinitiveScroll";
+import truncateString from "@/utils/truncateString";
 
-export const metadata: Metadata = {
-  title: "Search",
-  description: "",
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: { searchSlug: string };
+}) {
+  return {
+    title: `Search: ${truncateString(params.searchSlug, 17)}`,
+    description:
+      "Explore our intuitive product search page designed to help you quickly find exactly what you're looking for. Whether you're shopping for electronics, clothing, home goods, or more, our advanced filters and sorting options allow you to easily narrow down your choices and compare prices. Discover the best deals tailored to your preferences, and enjoy a hassle-free shopping experience with our user-friendly interface. Start your search now and find the perfect product that meets your needs and budget.",
+  };
+}
 
 interface PageProps {
   params: { searchSlug: string };
